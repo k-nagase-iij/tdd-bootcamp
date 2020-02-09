141a4107d4ac7f825f83b1aa6df8e1190231ef02
Kohei Nagase <k-nagase@iij.ad.jp>
2020-02-09 22:31:56 +0900

    Finished to chapter 3

--- src/main/java/money/Doller.java
+++ src/main/java/money/Doller.java
@@ -8,4 +8,9 @@ public class Doller {
     Doller times(int multiplier) {
         return new Doller(amount * multiplier);
     }
+    @Override
+    public boolean equals(Object object) {
+        Doller doller = (Doller) object;
+        return amount == doller.amount;
+    }
 }
--- src/test/java/money/MoneyTest.java
+++ src/test/java/money/MoneyTest.java
@@ -12,4 +12,9 @@ public class MoneyTest {
         product = five.times(3);
         assertEquals(15, product.amount);
     }
+    @Test
+    public void testEquality() {
+        assertTrue(new Doller(5).equals(new Doller(5)));
+        assertFalse(new Doller(5).equals(new Doller(6)));
+    }
 }
