37390b45e25dee5134715831417c2517059c7ddc
Kohei Nagase <k-nagase@iij.ad.jp>
2020-02-09 22:19:17 +0900

    Finished to chapter 2

---
+++ src/main/java/money/Doller.java
@@ -0,0 +1,11 @@
+package money;
+
+public class Doller {
+    int amount;
+    Doller(int amount) {
+        this.amount = amount;
+    }
+    Doller times(int multiplier) {
+        return new Doller(amount * multiplier);
+    }
+}
---
+++ src/test/java/money/MoneyTest.java
@@ -0,0 +1,15 @@
+package money;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+public class MoneyTest {
+    @Test
+    public void testMultiplication(){
+        Doller five = new Doller(5);
+        Doller product = five.times(2);
+        assertEquals(10, product.amount);
+        product = five.times(3);
+        assertEquals(15, product.amount);
+    }
+}
